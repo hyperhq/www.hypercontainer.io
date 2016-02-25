b62eafbf328bc2ebe93a0163163c090670e6f842
imeoer <imeoer@gmail.com>
2016-02-26 00:47:57 +0800

    add analytics.identify for website

--- website/index.js
+++ website/index.js
@@ -1,39 +1,26 @@
-import 'font-awesome/css/font-awesome.css';
-require('./normalize.css');
-require('./index.css');
+import 'font-awesome/css/font-awesome.css'
+require('./normalize.css')
+require('./index.css')
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     let playBtn = document.getElementById('play');
-//     let video = document.getElementById('video');
-//     video.pause();
-//     let hashChange = function() {
-//         if (location.hash != "#intro") {
-//             video.pause();
-//             playBtn.className = 'button';
-//         }
-//     }
-//     playBtn.addEventListener('click', function(event) {
-//         video.play();
-//         playBtn.className = 'button loading';
-//         event.preventDefault();
-//         event.stopPropagation();
-//     });
-//     video.addEventListener('click', function(event) {
-//         event.preventDefault();
-//         event.stopPropagation();
-//     });
-//     window.addEventListener('hashchange', hashChange);
-//     video.addEventListener('play', function(event) {
-//         window.location.hash = "#intro";
-//     });
-//     video.addEventListener('dblclick', function(event) {
-//         if (this.requestFullscreen) {
-//             this.requestFullscreen();
-//         } else if (this.mozRequestFullScreen) {
-//             this.mozRequestFullScreen();
-//         } else if (this.webkitRequestFullscreen) {
-//             this.webkitRequestFullscreen();
-//         }
-//     });
-//     hashChange();
-// });
+import uuid from 'uuid'
+
+const validEmail = function(email) {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const submit = document.getElementById('submit')
+    submit.addEventListener('click', function(event) {
+        const email = document.getElementById('email').value.trim()
+        if (validEmail(email)) {
+            const userId = analytics.user().anonymousId() || uuid.v4()
+            analytics.identify(userId, { email }, null, (err, ret) => {
+                alert('Subscribe Succeed')
+            })
+        } else {
+            alert('Invalid Emaill Address')
+        }
+        event.preventDefault()
+        event.stopPropagation()
+    })
+})
