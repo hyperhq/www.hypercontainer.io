d2f6cbdfd55a5ad57be7bd6d26ad4d034862fb2a
imeoer <imeoer@gmail.com>
2016-03-09 14:16:54 +0800

    update style

--- landing/index.js
+++ landing/index.js
@@ -1,11 +1,17 @@
-import 'font-awesome/css/font-awesome.css';
-require('./normalize.css');
-require('./index.css');
+import 'font-awesome/css/font-awesome.css'
+require('./normalize.css')
+require('./index.css')
+require('popups/css/popupS.css')
+import popups from 'popups'
 
 import uuid from 'uuid'
 
 const validEmail = function(email) {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
+}
+
+const popup = function(content) {
+  popups.alert({ content })
 }
 
 window.onSubmit = function() {
@@ -16,15 +22,15 @@ window.onSubmit = function() {
     const firstname = firstnameDom.value.trim()
     const lastname = lastnameDom.value.trim()
     if (!firstname) {
-        alert('Invalid Firstname')
+        popup('Invalid Firstname')
         return false
     }
     if (!lastname) {
-        alert('Invalid Lastname')
+        popup('Invalid Lastname')
         return false
     }
     if (!validEmail(email)) {
-        alert('Invalid Emaill Address')
+        popup('Invalid Emaill Address')
         return false
     }
     let userId = analytics.user().id()
@@ -38,7 +44,7 @@ window.onSubmit = function() {
         firstnameDom.value = ''
         lastnameDom.value = ''
         analytics.track('Requested Private Beta', {}, {}, () => {
-          alert('Subscribe Succeed')
+          popup('Subscribe Succeed')
         })
     })
 }
